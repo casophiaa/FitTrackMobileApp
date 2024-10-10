56624ced1fb3308b604f372c57b6a6035fc0a2a0
Allyssa Salgado <allyssa_salgado@dlsu.edu.ph>
2024-10-10 22:29:25 +0800

    intro page but pictures are trial lang since from tut

--- app/src/main/java/com/example/fittrackmobileapp/MainActivity.java
+++ app/src/main/java/com/example/fittrackmobileapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.fittrackmobileapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,10 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void btnClicked(View v){
+        Intent i = new Intent(MainActivity.this, ViewPager.class);
+        startActivity(i);
     }
 }
---
+++ app/src/main/java/com/example/fittrackmobileapp/ViewPagerItem.java
@@ -0,0 +1,13 @@
+package com.example.fittrackmobileapp;
+
+public class ViewPagerItem {
+
+    int imageID;
+    String heading, description;
+
+    public ViewPagerItem(int imageID, String heading, String description) {
+        this.imageID = imageID;
+        this.heading = heading;
+        this.description = description;
+    }
+}
