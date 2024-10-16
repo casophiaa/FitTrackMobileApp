c96bb9ec5c42b712f84337754bed1a302e5ff611
Allyssa Salgado <allyssa_salgado@dlsu.edu.ph>
2024-10-16 19:08:21 +0800

    90% finish dashboard

--- app/src/main/java/com/example/fittrackmobileapp/Dashboard.java
+++ app/src/main/java/com/example/fittrackmobileapp/Dashboard.java
@@ -1,24 +1,112 @@
 package com.example.fittrackmobileapp;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
 
 public class Dashboard extends AppCompatActivity {
+
+    RecyclerView horizontalRv;
+    ArrayList<Item> dataSource;
+    LinearLayoutManager linearLayoutManager;
+    MyRvAdapter myRvAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_dashboard);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        horizontalRv = findViewById(R.id.horizontalRv);
+        String currentDate = new SimpleDateFormat("MMMM dd, yyyy", Locale.getDefault()).format(new Date());
+        dataSource = new ArrayList<>();
+        dataSource.add(new Item(currentDate, R.drawable.a));
+        dataSource.add(new Item(currentDate, R.drawable.fimmies2));
+        dataSource.add(new Item(currentDate, R.drawable.fimmies1));
+
+        linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        myRvAdapter = new MyRvAdapter(dataSource);
+        horizontalRv.setLayoutManager(linearLayoutManager);
+        horizontalRv.setAdapter(myRvAdapter);
+    }
+
+    public static class Item {
+        private String title;
+        private int imageResId;
+
+        public Item(String title, int imageResId) {
+            this.title = title;
+            this.imageResId = imageResId;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public int getImageResId() {
+            return imageResId;
+        }
+    }
+
+    class MyRvAdapter extends RecyclerView.Adapter<MyRvAdapter.MyHolder> {
+        ArrayList<Item> data;
+
+        public MyRvAdapter(ArrayList<Item> data) {
+            this.data = data;
+        }
+
+        @NonNull
+        @Override
+        public MyHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(Dashboard.this).inflate(R.layout.item_card_view, parent, false);
+            return new MyHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull MyHolder holder, int position) {
+            Item item = data.get(position);
+            holder.tvTitle.setText(item.getTitle());
+            holder.image.setImageResource(item.getImageResId());
+        }
+
+        @Override
+        public int getItemCount() {
+            return data.size();
+        }
+
+        class MyHolder extends RecyclerView.ViewHolder {
+            TextView tvTitle;
+            ImageView image;
+
+            public MyHolder(@NonNull View itemView) {
+                super(itemView);
+                tvTitle = itemView.findViewById(R.id.tvTitle);
+                image = itemView.findViewById(R.id.image);
+            }
+        }
     }
 }
+
