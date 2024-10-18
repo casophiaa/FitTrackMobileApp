37801d1f5e9d76baf016c612e4553f25e6beb0c0
kaliusolence <luisa_jacob@dlsu.edu.ph>
2024-10-18 09:31:32 +0800

    added date logic in prog page

--- app/src/main/java/com/example/fittrackmobileapp/ProgressPage.java
+++ app/src/main/java/com/example/fittrackmobileapp/ProgressPage.java
@@ -2,6 +2,7 @@ package com.example.fittrackmobileapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ProgressBar;
 import androidx.activity.EdgeToEdge;
@@ -11,8 +12,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 
@@ -29,6 +33,15 @@ public class ProgressPage extends AppCompatActivity{
         // will implement fetching user action data to show progress (i.e. steps, exercises, etc.)
         progressBar = findViewById(R.id.progressBar);
 
+        //for the dates
+        //date = findViewById(R.id.);
+
+        Date currentTime = Calendar.getInstance().getTime();
+        String formattedDate = DateFormat.getDateInstance(DateFormat.FULL).format(currentTime);
+
+        String[] splitDate = formattedDate.split(",");
+
+        //date.setText(splitDate[1]); //to get month & day
     }
 
 
