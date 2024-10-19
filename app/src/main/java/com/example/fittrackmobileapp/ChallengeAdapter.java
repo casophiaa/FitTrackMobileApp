108bf694a8f531f66ff304721153d9b24177422f
kaliusolence <luisa_jacob@dlsu.edu.ph>
2024-10-19 19:37:16 +0800

    added new and wip challenge files

---
+++ app/src/main/java/com/example/fittrackmobileapp/ChallengeAdapter.java
@@ -0,0 +1,27 @@
+package com.example.fittrackmobileapp;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class ChallengeAdapter extends RecyclerView.Adapter<ChallengeAdapter.MyHolder>{
+    ArrayList<ChallengeItem> data;
+
+    @Override
+    public ChallengeAdapter.MyHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ChallengeAdapter.MyHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
---
+++ app/src/main/java/com/example/fittrackmobileapp/ChallengeItem.java
@@ -0,0 +1,13 @@
+package com.example.fittrackmobileapp;
+
+public class ChallengeItem {
+
+    int imageSource;
+    String day, exercise, desc;
+
+    public ChallengeItem(String day, String exercise, String desc, int imageSource){
+        this.day = day;
+        this.exercise =exercise;
+        this.day = day;
+    }
+}
